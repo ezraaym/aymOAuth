5dc335ce1094defeacc93c95534bcea2c3393188
Ezra Haugabrooks <ezra@aymscores.com>
2024-10-21 19:26:22 -0400

    Cleanup: Removed unnecessary files and reset plugin structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# aymOAuth
-handle multiple social media logins and provide login buttons and forms.
--- assets/js/social-auth-admin.js
+++
@@ -1,4 +0,0 @@
-// JavaScript for handling dynamic elements in the dashboard
-jQuery(document).ready(function($) {
-    console.log('Social Media Auth Dashboard loaded.');
-});
